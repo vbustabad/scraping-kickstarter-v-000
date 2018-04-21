23a0952cb260efed7d6cc5b8a853cd7e2593b98a
Veronica Bustabad <vbustabad@gmail.com>
2018-04-21 03:24:50 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -25,7 +25,7 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
+
+  # return the projects hash
   projects
 end
-
-create_project_hash
